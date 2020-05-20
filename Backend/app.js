3fc197cbb5479d9f7e219897a4e6c5ea17ffe614
Vicenteefenequis <efenequis19981@gmail.com>
2020-05-19 22:59:49 -0300

    Api de login aplication

---
+++ Backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const userRouter = require('./routes/userRouter')
+const adminRouter = require('./routes/adminRouter')
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL,
+    {
+        useNewUrlParser:true, 
+        useUnifiedTopology: true
+    },
+    (err)=>{
+        if(err)
+        console.log(err)
+        else
+        console.log('Mongo Connected')
+    })
+
+
+app.use('/user',express.json(),userRouter)
+
+
+app.use('/admin',express.json(),adminRouter)
+
+app.listen(process.env.PORT,(err)=>{
+    if(err)
+    console.log(err)
+    else
+    console.log('Running')
+})
+
---
+++ Backend/routes/adminRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../controllers/authController')
+
+router.get('/',auth,(req,res)=>{
+    if(req.user.admin)
+    res.send('Esse dado só deve ser visto pelo Admin')
+    else res.status(401).send('Not Admin: Acess Denied')
+})
+
+module.exports = router
